688187135f1f798c81b1007764c7d0a1b208492c
Guilherme Costi <guilherme.costi@lambda3.com.br>
2020-04-02 19:26:30 -0300

    fixed docker port

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import sqlite3
 import yaml
 import pandas as pd
@@ -54,4 +55,9 @@ def get_politic_party(id):
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        port = 5000
+    
+    app.run(host='0.0.0.0', port=port, debug=True)
